bdd18028f0b858654eaf6f6307bbac19f984f454
Omar M <o.malagouen@technologyandstrategy.com>
2023-01-06 14:38:34 +0100

    Separate View and ViewController

--- Track Me/Sources/Controllers/ViewController.swift
+++ Track Me/Sources/Controllers/ViewController.swift
@@ -8,39 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let mainView = UIView()
-    private let label = UILabel()
+    private let mainView = UserLocationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setupMainView()
-        setupConstraints()
     }
 
     override func loadView() {
         mainView.backgroundColor = .yellow
         view = mainView
-    }
-
-    private func setupMainView() {
-        label.text = "My label in code behind in order to avoid all git merge conflict when multiple persons works on the same code at the same time."
-        label.numberOfLines = 0
-        mainView.addSubview(label)
-    }
-
-    private func setupConstraints() {
-        label.translatesAutoresizingMaskIntoConstraints = false
-        mainView.translatesAutoresizingMaskIntoConstraints = false
-        let safeArea = mainView.safeAreaLayoutGuide
-
-        mainView.addSubview(label)
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
-            safeArea.trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: 10),
-            label.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
-            safeArea.bottomAnchor.constraint(greaterThanOrEqualTo: label.bottomAnchor, constant: 10),
-        ])
     }
 }
 
--- Track Me/Sources/Views/UserLocationView.swift
+++ Track Me/Sources/Views/UserLocationView.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UIKit
+
+class UserLocationView: UIView {
+    private enum Constants {
+        static let cornerRadius: CGFloat = 12
+        public static let buttonSize = CGSize(width: 50, height: 50)
+        static let buttonImageInset: CGFloat = 10
+    }
+
+    private let label = UILabel()
+
+    init() {
+        super.init(frame: .zero)
+
+        setupViews()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        backgroundColor = .systemPink
+        label.text = "My label in code behind in order to avoid all git merge conflict when multiple persons works on the same code at the same time."
+        label.numberOfLines = 0
+        addSubview(label)
+    }
+
+    private func setupConstraints() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: 10),
+            label.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
+            safeAreaLayoutGuide.bottomAnchor.constraint(greaterThanOrEqualTo: label.bottomAnchor, constant: 10),
+        ])
+    }
+}
